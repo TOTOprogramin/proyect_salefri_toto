9b7b497aba584d38127e393a2dc3fad847ccfe52
TOTOprogramin <125613287+TOTOprogramin@users.noreply.github.com>
2023-02-16 17:35:06 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const yesBtn= document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('jejeje gogogogo entralee')
+
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover',function () {
+   const randomX = parseInt(Math.random()*100);
+   const randomY = parseInt(Math.random()*100);
+   noBtn.style.setProperty('top',randomY+'%');
+   noBtn.style.setProperty('left',randomX+'%');
+   noBtn.style.setProperty('transform',`traslate(-${randomX}%,-${randomY}%)`);
+
+})
